c2f2b960399cef5404142b25640e84a924ce7265
DDelgadoD <david.delgado82@gmail.com>
2022-07-06 10:32:16 +0200

    Totally functional

--- api/urls.py
+++ api/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from .views import ProductApiView, CartAddView, CartApiView, CheckoutApiView
 
 urlpatterns = [
-    path('product_list/', ProductApiView.as_view()),
+    path('product_list/', ProductApiView.as_view(), name='product_list'),
     path('add_to_cart/<int:product_id>', CartAddView.as_view()),
     path('add_to_cart/<int:product_id>/<int:quantity>', CartAddView.as_view()),
     path('add_to_cart/<int:product_id>/<str:cart>', CartAddView.as_view()),
     path('add_to_cart/<int:product_id>/<int:quantity>/<str:cart>', CartAddView.as_view()),
-    path('cart/<str:creation_date>', CartApiView.as_view()),
-    path('checkout/<str:creation_date>', CheckoutApiView.as_view()),
+    path('cart/<str:creation_date>', CartApiView.as_view(), name='cart'),
+    path('checkout/<str:creation_date>', CheckoutApiView.as_view(), name='checkout'),
 ]
