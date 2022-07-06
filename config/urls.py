7cf253f2b93286f61af04fa6b8abab9941ec67be
DDelgadoD <david.delgado82@gmail.com>
2022-07-06 11:01:34 +0200

    Totally functional

--- config/urls.py
+++ config/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
-    path('', TemplateView.as_view(template_name='home.html'), name= 'home'),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
